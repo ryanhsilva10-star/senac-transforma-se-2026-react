3e8d0818788daf7541e6e9b7c15e0a7b6ba2c449
ryanhsilva10-star <ryan.h.silva10@aluno.senai.br>
2026-09-11 18:57:04 -0300

    aula 10/09

--- curso-react/src/pages/Painel.jsx
+++ curso-react/src/pages/Painel.jsx
@@ -6,7 +6,8 @@ function Painel() {
     const [user, setUser] = useState({});
     const [users, setUsers] = useState([]);
     const [logged, setLogged] = useState({});
-
+    const [isEdit, setIsEdit] = useState(false);
+    const [index, setIndex] = useState(-1);
     useEffect(() => {
         const logged = JSON.parse(localStorage.getItem('logged'))
         setLogged(logged)
@@ -14,8 +15,8 @@ function Painel() {
 
     useEffect(() => {
         const tempUsers = JSON.parse(localStorage.getItem('users'))
-        if(tempUsers) setUsers(tempUsers)
-    } , [])
+        if (tempUsers) setUsers(tempUsers)
+    }, [])
 
 
     function handleRegister() {
@@ -47,83 +48,96 @@ function Painel() {
                         <h2>Cadastre um novo usuário</h2>
                         <p>Preencha as informações abaixo</p>
 
-                        <form className=" flex flex-col">
-                            Nome:
-                            <input
-                                type="text"
-                                value={user.nome}
-                                placeholder="Digite seu nome completo"
-                                onChange={(e) => setUser({ ...user, nome: e.target.value })}
 
-                            />
-                            Email:
-                            <input
-                                type="email"
-                                value={user.email}
-                                placeholder="Digite o seu melhor email"
-                                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                            />
-                            Senha:
-                            <input
-                                type="password"
-                                placeholder="Letra maiúscula e números"
-                                onChange={(e) => setUser({ ...user, senha: e.target.value })}
-                            />
-                            Data de nascimento:
-                            <input
-                                type="date" onChange={(e) => setUser({ ...user, nascimento: e.target.value })}
-                            />
+                        { isEdit ? (
+            <form className="flex flex-col">
+                Nome:
+                <input value={user.nome} onChange={ (e) => setUser({...user, nome: e.target.value }) } type="text" placeholder="Digite seu nome completo" />
+                Email:
+                <input value={user.email} onChange={ (e) => setUser({...user, email: e.target.value }) }  type="email" placeholder="Digite o seu melhor email" />
+    
+                Senha:
+                <input onChange={ (e) => setUser({...user, senha: e.target.value }) }  type="password" placeholder="Letra maiúscula e números" />
+                Data de nascimento:
+                <input value={user.nascimento} onChange={ (e) => setUser({...user, nascimento: e.target.value }) }  type="date" />
+       
+                { index != -1 && (
+                <a onClick={()=> setIsEdit(false)} 
+                    className="mt-5 text-black text-center 
+                    rounded-md py-2 bg-red-300"
+                >
+                    Cancelar
+                </a>
+                )
+                }
 
-                            <a onClick={handleRegister}
-                                className="mt-5 bg-primary text-white text-center rounded-md py-2">Salvar</a>
-                        </form>
-                    </div>
-                </div>)
+                <a onClick={handleRegister} 
+                    className="mt-5 bg-primary text-white text-center rounded-md py-2"
+                >
+                    Salvar
+                </a>
+            </form>): //else 
+            (
+                <>
+                  <p> Nome: {user.nome}</p>
+                  <p> Email: {user.email}</p>
+                  <p> Data de Nascimento: {user.nascimento}</p>
+                  <a onClick={()=> setIsEdit(true)} className="mt-5 bg-primary text-black text-center rounded-md py-2 bg-yellow-500">Editar</a>
+                </>    
+            )
             }
 
-            <a onClick={() => setModal(true)} className="rounded-full bg-blue-100 text-white px-4 py-3 fixed bottom-0 right-0"> + </a>
-
-            <table>
-                <thead>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Ações</th>
-                </thead>
-                <tbody className="font-secundary">
-                    {users.map(u => (
-                        <tr>
-                            <td>{u.nome}</td>
-                            <td>{u.email}</td>
-                            <td>
-                                <a className='cursor-pointer
-                                px-2
-                                max-4
-                                horver:shadow
-                                shadow-md
-                                text-white
-                                rounded-full
-                                bg-green-500'
-                                    onClick={() => updateUser(u)}>V</a>
-
-                                <a className='cursor-pointer
-                                px-2
-                                max-4
-                                horver:shadow
-                                shadow-md
-                                text-white
-                                rounded-full
-                                bg-green-500'
-                                >X</a>
-
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-
         </div>
-    );
+    </div>
+)}
+
+<a onClick={() => {
+    setModal(true)
+    setIsEdit(true)
+}}
+                            className="rounded-full bg-primary text-white 
+    px-4 py-3 fixed bottom-0 right-0"> + </a>
+
+    <table>
+        <thead>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+        </thead>
+        <tbody className="font-secundary">
+            {users.map( (u,i) => (
+                <tr>
+                    <td>{u.nome}</td>
+                    <td>{u.email}</td>
+                    <td>
+                        <a className='cursor-pointer
+                                       px-2
+                                       mx-4
+                                       hover:shadow
+                                       shadow-md
+                                       text-white
+                                       rounded-full
+                                       bg-green-500'
+                            onClick={()=> updateUser(i)}
+                        >V</a>
+                        <a className='cursor-pointer
+                                       px-2
+                                       mx-4
+                                       hover:shadow
+                                       shadow-md
+                                       text-white
+                                       rounded-full
+                                       bg-red-500'
+                            onClick={()=> deleteUser(i)}
+                        >X</a>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+
+</div>
+    )
 }
 
 export default Painel;
